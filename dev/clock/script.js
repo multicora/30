d3cd287fdb67e59155b31931a46c862002e29cd8
Dmytro Borchuk <Dmytro.Borchuk@symphony-solutions.eu>
2016-12-15 13:13:26 +0200

    Added clock section

---
+++ dev/clock/script.js
@@ -0,0 +1,13 @@
+$('#clock-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  variableWidth: true,
+  arrows: false
+});
+
+$('#screen-shots-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  variableWidth: true,
+  arrows: false
+})
